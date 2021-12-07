89df1cd4caacc6d58bb20ad5b30d9b75dd4df73f
nazarenko_vv <nazarenko_vv@magnit.ru>
2021-12-07 21:48:06 +0000

    Fully migrated to java-based config from web.xml

--- module-2/src/main/java/org/example/WebAppInitializer.java
+++ module-2/src/main/java/org/example/WebAppInitializer.java
@@ -1,16 +1,38 @@
 package org.example;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.*;
 
 public class WebAppInitializer implements WebApplicationInitializer {
+
+	private final Logger log = Logger.getLogger(WebAppInitializer.class);
+
 	@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
+	public void onStartup(ServletContext servletContext) {
+		log.info("Start onStartup");
+
+		log.info("Start loading app config");
+		/* App config loading and listener definition */
 		XmlWebApplicationContext appContext = new XmlWebApplicationContext();
 		appContext.setConfigLocation("classpath:app-config.xml");
 		servletContext.addListener(new ContextLoaderListener(appContext));
+
+		log.info("Start loading web config");
+		XmlWebApplicationContext webContext = new XmlWebApplicationContext();
+		webContext.setConfigLocation("classpath:web-config.xml");
+
+		/* Define dispatcherServlet	*/
+		DispatcherServlet dispatcherServlet = new DispatcherServlet(webContext);
+		/* Register dispatcherServlet */
+		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", dispatcherServlet);
+		dispatcher.setLoadOnStartup(1); // need to be loaded and started at first
+		dispatcher.addMapping("/");
+
+		log.info("Finish loading config");
 	}
 }
